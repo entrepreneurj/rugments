0cfbebbd4c874ed0223ec1f165f5344a90979c38
Stefan Tatschner <stefan@sevenbyte.org>
2015-01-01 20:22:58 +0100

    Removed === operator

--- lib/rugments/text_analyzer.rb
+++ lib/rugments/text_analyzer.rb
@@ -1,11 +1,11 @@
 module Rugments
   class TextAnalyzer < String
-    # Find a shebang.  Returns nil if no shebang is present.
+    # Find a shebang. Returns nil if no shebang is present.
     def shebang
-      return @shebang if instance_variable_defined? :@shebang
+      return @shebang if instance_variable_defined?(:@shebang)
 
       self =~ /\A\s*#!(.*)$/
-      @shebang = $1
+      @shebang = Regexp.last_match[1]
     end
 
     # Check if the given shebang is present.
@@ -13,24 +13,23 @@ module Rugments
     # This normalizes things so that `text.shebang?('bash')` will detect
     # `#!/bash`, '#!/bin/bash', '#!/usr/bin/env bash', and '#!/bin/bash -x'
     def shebang?(match)
-      match = /\b#{match}(\s|$)/
-      match === shebang
+      shebang =~ /\b#{match}(\s|$)/ ? true : false
     end
 
     # Return the contents of the doctype tag if present, nil otherwise.
     def doctype
-      return @doctype if instance_variable_defined? :@doctype
+      return @doctype if instance_variable_defined?(:@doctype)
 
-      self =~ %r(\A\s*
+      self =~ %r{\A\s*
         (?:<\?.*?\?>\s*)? # possible <?xml...?> tag
         <!DOCTYPE\s+(.+?)>
-      )xm
-      @doctype = $1
+            }xm
+      @doctype = Regexp.last_match[1]
     end
 
     # Check if the doctype matches a given regexp or string
-    def doctype?(type=//)
-      type === doctype
+    def doctype?(type = //)
+      doctype =~ type ? true : false
     end
 
     # Return true if the result of lexing with the given lexer contains no
