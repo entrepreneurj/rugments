bef5e89f8a8383e242b3a0f1b9ee857c2521ee5a
Stefan Tatschner <stefan@sevenbyte.org>
2015-01-14 11:19:09 +0100

    fixed highlight of monokai theme for diff lexer
    Merged 41b1c035209e1626db994781bb42796a46eedaab

--- lib/rugments/themes/monokai.rb
+++ lib/rugments/themes/monokai.rb
@@ -3,24 +3,26 @@ module Rugments
     class Monokai < CSSTheme
       name 'monokai'
 
-      palette black: '#000000'
-      palette bright_green: '#a6e22e'
-      palette bright_pink: '#f92672'
-      palette carmine: '#960050'
-      palette dark: '#49483e'
-      palette dark_grey: '#888888'
-      palette dark_red: '#aa0000'
-      palette dimgrey: '#75715e'
-      palette emperor: '#555555'
-      palette grey: '#999999'
-      palette light_grey: '#aaaaaa'
-      palette light_violet: '#ae81ff'
-      palette soft_cyan: '#66d9ef'
-      palette soft_yellow: '#e6db74'
-      palette very_dark: '#1e0010'
-      palette whitish: '#f8f8f2'
-      palette orange: '#f6aa11'
-      palette white: '#ffffff'
+      palette black:            '#000000'
+      palette bright_green:     '#a6e22e'
+      palette bright_pink:      '#f92672'
+      palette carmine:          '#960050'
+      palette dark:             '#49483e'
+      palette dark_grey:        '#888888'
+      palette dark_red:         '#aa0000'
+      palette dimgrey:          '#75715e'
+      palette dimgreen:         '#324932'
+      palette dimred:           '#493131'
+      palette emperor:          '#555555'
+      palette grey:             '#999999'
+      palette light_grey:       '#aaaaaa'
+      palette light_violet:     '#ae81ff'
+      palette soft_cyan:        '#66d9ef'
+      palette soft_yellow:      '#e6db74'
+      palette very_dark:        '#1e0010'
+      palette whitish:          '#f8f8f2'
+      palette orange:           '#f6aa11'
+      palette white:            '#ffffff'
 
       style Comment,
             Comment::Multiline,
@@ -28,8 +30,8 @@ module Rugments
       style Comment::Preproc,                 fg: :dimgrey, bold: true
       style Comment::Special,                 fg: :dimgrey, italic: true, bold: true
       style Error,                            fg: :carmine, bg: :very_dark
-      style Generic::Deleted,
-            Generic::Inserted,                fg: :black
+      style Generic::Inserted,                fg: :white, bg: :dimgreen
+      style Generic::Deleted,                 fg: :white, :bg: :dimred
       style Generic::Emph,                    fg: :black, italic: true
       style Generic::Error,
             Generic::Traceback,               fg: :dark_red
