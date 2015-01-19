5a0e3c2ed6bb5de24df5e8dfad4cab74e53fec14
Stefan Tatschner <stefan@sevenbyte.org>
2015-01-19 11:34:03 +0100

    Added testcases for Rugments::Lexer

--- spec/lexers_spec.rb
+++ spec/lexers_spec.rb
@@ -16,4 +16,49 @@ describe Rugments::Lexer do
       expect(lexer.new).to be_an_instance_of(Rugments::Lexers::C)
     end
   end
+
+  describe '::tag' do
+    it 'returns the unique identifier' do
+      lexer = Rugments::Lexer.find_by_name('ruby')
+      expect(lexer.tag.to_s).to eq('ruby')
+    end
+  end
+
+  describe '::title' do
+    it 'returns the human readable title' do
+      lexer = Rugments::Lexer.find_by_name('ruby')
+      expect(lexer.title).to eq('Ruby')
+    end
+  end
+
+  describe '::desc' do
+    it 'returns the human readable description' do
+      lexer = Rugments::Lexer.find_by_name('ruby')
+      expect(lexer.desc).to eq('The Ruby programming language (ruby-lang.org)')
+    end
+  end
+
+  describe '::aliases' do
+    it 'returns the aliases array' do
+      lexer = Rugments::Lexer.find_by_name('ruby')
+      expect(lexer.aliases).to eq(%w(rb))
+    end
+  end
+
+  describe '::filenames' do
+    it 'returns the filenames array' do
+      lexer = Rugments::Lexer.find_by_name('ruby')
+      expect(lexer.filenames).to eq(
+        %w(*.rb *.ruby *.rbw *.rake *.gemspec *.podspec Rakefile Guardfile
+           Gemfile Capfile Podfile Vagrantfile *.ru *.prawn)
+      )
+    end
+  end
+
+  describe '::mimetypes' do
+    it 'returns the mimetypes array' do
+      lexer = Rugments::Lexer.find_by_name('ruby')
+      expect(lexer.mimetypes).to eq(%w(text/x-ruby application/x-ruby))
+    end
+  end
 end
